2a9deacc722d9381ad731eb0dba847be59134275
Maksim Romaniv <maxim.romaniv@netent.com>
2015-05-29 11:38:19 +0300

    [gulp] Added compile config

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@ var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
+var htmlreplace = require('gulp-html-replace');
 
 gulp.task("default", function () {
-    return gulp.src("javascript/**/*.js")
+    return gulp.src("app/client/js/**/*.js")
         .pipe(sourcemaps.init())
         .pipe(concat("all.js"))
         .pipe(babel())
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest("dist/client/js"));
+});
+
+gulp.task("htmlCompile", function () {
+    return gulp.src("app/client/index.html")
+        .pipe(htmlreplace({
+            'js': 'js/all.js'
+        }))
+        .pipe(gulp.dest("dist/client"));
 });
